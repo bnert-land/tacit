58597e61df66018093cfae3cf8cecaf425bb7faa
Brent Soles (@bnert) <brent@soles.dev>
2023-09-28 13:58:31 -0400

    core.async patching

--- dev/user.clj
+++ dev/user.clj
@@ -14,4 +14,20 @@
     (t/virtual-let [value "{:msg \"hello\"}"]
       (edn/read-string value))
     deref)
+
+
+  (require 'tacit.async :reload)
+  (tacit.async/use-virtual-thread-executor!)
+
+  (require '[clojure.core.async :refer [chan put! poll! go go-loop >! <!]])
+
+  (def c (chan))
+
+  (dotimes [i 1000]
+    (put! c {:value i}))
+
+  (go-loop []
+    (let [v? (<! c)]
+      (println "V?>" v?)
+      (recur)))
 )

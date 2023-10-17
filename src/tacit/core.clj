7253dfd83a072c4777b807203e1667ec511b8439
Brent Soles (@bnert) <brent@soles.dev>
2023-10-17 12:26:05 -0400

    chore: add optimizations and guardrails

--- src/tacit/core.clj
+++ src/tacit/core.clj
@@ -9,12 +9,17 @@
 
 ; Took inspiration from `(future)` macro/call stack
 ; other method wasn't actually working/executing asynchronously
-(defn virtual-call [f]
+(defn virtual-call
+  "Takes a function and executes it on the module level virtual thread pool
+  executor. Returns java.util.concurrent.Future, which can be dereferenced
+  in order to get result value"
+  {:static true}
+  [f]
   (.submit ^java.util.concurrent.ExecutorService executor*
            ^java.util.concurrent.Callable f))
 
 (defmacro virtual-let [bindings & body]
-  `(tacit.core/virtual-call (fn* [] (let ~bindings ~@body))))
+  `(tacit.core/virtual-call (^{:once true} fn* [] (let ~bindings ~@body))))
 
 (defmacro virtual [& body]
   `(tacit.core/virtual-let [] ~@body))
